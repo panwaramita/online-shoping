c271f6be6dfeca60f234ee888551ccbc7cc9c941
Amita <panwar.amita@gmail.com>
2022-05-31 12:28:52 -0400

    modification in package.json

--- server.js
+++ server.js
@@ -12,15 +12,14 @@ ConnectionDB();
 const PORT = process.env.PORT || 5000;
 __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.send(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API IS RUNNING");
-  });
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 }
 
 app.use(express.json());
