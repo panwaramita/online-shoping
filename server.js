bead1b156324efc38ee685de8247d402b1af22c2
Amita <panwar.amita@gmail.com>
2022-05-31 12:15:28 -0400

    created the component folder

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const path = require("path");
+
 app.use(cors());
 const ConnectionDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 ConnectionDB();
 
 const PORT = process.env.PORT || 5000;
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
+}
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
