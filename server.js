9d6a03a2f3faadfe7be370c0a469225f6c5ce3e3
Amita <panwar.amita@gmail.com>
2022-05-31 12:46:01 -0400

    modification in server file

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require("cors");
-const app = express();
 const path = require("path");
 
-app.use(cors());
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
 const ConnectionDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 ConnectionDB();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
-__dirname = path.resolve();
-
-// ... other app.use middleware
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.use(express.static(__dirname + "/"));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
 });
 
-app.use(express.json());
-app.use("/api/products", productRoutes);
 app.listen(PORT, () => {
   console.log(`server listing to the port ${PORT}`);
 });
