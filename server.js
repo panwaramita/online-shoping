5c3c98b1e45431942fccac6bb3236e057a1b0c96
Amita <panwar.amita@gmail.com>
2022-05-31 14:11:25 -0400

    modification in server file

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/products", productRoutes);
 
-ConnectionDB();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 app.use(express.static(__dirname + "/"));
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "client/build/index.html"));
-// });
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "client/build/index.html"));
+});
+ConnectionDB();
 
 app.listen(PORT, () => {
   console.log(`server listing to the port ${PORT}`);
